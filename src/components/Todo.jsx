d29e9964b24dd5f7e3f54dea77cf50ba64d09fda
viktoria naskova <smorodinka-gg@mail.ru>
2026-01-22 17:53:05 +0300

    feat: add logic for delete all tasks, add new tasks, change  task's state (complete/not complete)

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,39 @@
+import { useState } from "react";
 import AddTaskForm from "./AddTaskForm";
 import SearchTaskForm from "./SearchTaskForm";
 import TodoInfo from "./TodoInfo";
 import TodoList from "./TodoList";
 
 const Todo = () => {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     { id: "task-1", title: "Купить молоко", isDone: false },
     { id: "task-2", title: "Погладить кота", isDone: true },
-  ];
+  ]);
+
+  const [newTaskTitle, setNewTaskTitle] = useState("");
 
   const deleteAllTasks = () => {
-    console.log("Hello");
+    const isConfirmed = confirm("Are you sure you want to delete all?");
+
+    if (isConfirmed) {
+      setTasks([]);
+    }
   };
 
   const deleteTask = (taskId) => {
-    console.log(`Hello ${taskId}`);
+    setTasks(tasks.filter((task) => task.id !== taskId));
   };
 
   const toggleTaskComplete = (taskId, isDone) => {
-    console.log(`Hello ${taskId} ${isDone}`);
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === taskId) {
+          return { ...task, isDone };
+        }
+
+        return task;
+      }),
+    );
   };
 
   const filterTasks = (query) => {
@@ -26,13 +41,26 @@ const Todo = () => {
   };
 
   const addTask = () => {
-    console.log("Задача добавлена");
+    if (newTaskTitle.trim().length > 0) {
+      const newTask = {
+        id: crypto?.randomUUID() ?? Date.now().toString,
+        title: newTaskTitle,
+        isDone: false,
+      };
+
+      setTasks([...tasks, newTask]);
+      setNewTaskTitle("");
+    }
   };
 
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
-      <AddTaskForm addTask={addTask} />
+      <AddTaskForm
+        addTask={addTask}
+        newTaskTitle={newTaskTitle}
+        setNewTaskTitle={setNewTaskTitle}
+      />
       <SearchTaskForm onSearchInput={filterTasks} />
       <TodoInfo
         total={tasks.length}
