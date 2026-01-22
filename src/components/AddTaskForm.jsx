1675a8530206622fde606c324ae8af8a4032eea0
viktoria naskova <smorodinka-gg@mail.ru>
2026-01-22 15:14:00 +0300

    refactor: add props for Button, Field, SearchTaskForm, Todo, TodoInfo, TodoItem, TodoList, add base logic of events onClick, onSubmit, onInput

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,13 +1,20 @@
-import Field from './Field'
-import Button from './Button'
+import Field from "./Field";
+import Button from "./Button";
 
-const AddTaskForm = () => {
+const AddTaskForm = (props) => {
+  const { addTask } = props;
+
+  const onSubmit = () => {
+    event.preventDefault();
+    addTask();
+  }
+
   return (
-    <form className="todo__form">
-      <Field />
-      <Button />
+    <form className="todo__form" onSubmit={onSubmit}>
+      <Field className="todo__field" label="New task title" id="new-task" />
+      <Button type="submit">Add</Button>
     </form>
-  )
-}
+  );
+};
 
-export default AddTaskForm
+export default AddTaskForm;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,51 @@
-import AddTaskForm from './AddTaskForm'
-import SearchTaskForm from './SearchTaskForm'
-import TodoInfo from './TodoInfo'
-import TodoList from './TodoList'
+import AddTaskForm from "./AddTaskForm";
+import SearchTaskForm from "./SearchTaskForm";
+import TodoInfo from "./TodoInfo";
+import TodoList from "./TodoList";
 
 const Todo = () => {
+  const tasks = [
+    { id: "task-1", title: "Купить молоко", isDone: false },
+    { id: "task-2", title: "Погладить кота", isDone: true },
+  ];
+
+  const deleteAllTasks = () => {
+    console.log("Hello");
+  };
+
+  const deleteTask = (taskId) => {
+    console.log(`Hello ${taskId}`);
+  };
+
+  const toggleTaskComplete = (taskId, isDone) => {
+    console.log(`Hello ${taskId} ${isDone}`);
+  };
+
+  const filterTasks = (query) => {
+    console.log(`поиск ${query}`);
+  };
+
+  const addTask = () => {
+    console.log("Задача добавлена");
+  };
+
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
-      <AddTaskForm />
-      <SearchTaskForm />
-      <TodoInfo />
-      <TodoList />
+      <AddTaskForm addTask={addTask} />
+      <SearchTaskForm onSearchInput={filterTasks} />
+      <TodoInfo
+        total={tasks.length}
+        done={tasks.filter(({ isDone }) => isDone).length}
+        onDeleteAllButtonClick={deleteAllTasks}
+      />
+      <TodoList
+        tasks={tasks}
+        onDeleteTaskButtonClick={deleteTask}
+        onTaskCompleteChange={toggleTaskComplete}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
